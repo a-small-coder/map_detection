39c05e49f4fe7d5d519443c0519bd56c27cf15ee
master <hulktv222@mail.ru>
2022-06-06 14:04:57 +0300

    created table auth main pages

---
+++ src/main/resources/static/js/chart.js
@@ -0,0 +1,19 @@
+//pie
+var ctxP = document.getElementById("pieChart").getContext('2d');
+var myPieChart = new Chart(ctxP, {
+  type: 'pie',
+  data: {
+    labels: ["Сделано", "В работе", "Ожидает"],
+    datasets: [{
+      data: [10, 30, 80],
+      backgroundColor: ["#46BFBD", "#FDB45C", "#F7464A"],
+      hoverBackgroundColor: ["#5AD3D1", "#FFC870", "#FF5A5E"]
+    }]
+  },
+  options: {
+    legend: {
+        display: false
+    },
+    responsive: true
+  }
+});
